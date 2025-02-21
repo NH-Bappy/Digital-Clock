249e09b9dab12c8423af5b391964397bb2d55fe3
NH-Bappy <naimulbappy2207@gmail.com>
2025-02-22 01:11:39 +0600

    complete

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -12,21 +12,35 @@ const DigitalClock = () => {
         },1000);
 
         return() => {
-            clearInterval();
+            clearInterval(intervalId);
         }
 
     },[]);
 
 
 
+    function formatTime(){
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        let meridiem = hours >= 12 ? "PM":"AM";
 
+        hours = hours % 12 || 12;
+
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+
+
+
+        return `${hours}:${minutes}:${seconds} ${meridiem}`;
+    }
 
 
 
 
   return (
     <div>
-        <h2>00:00:00</h2>
+        <h2>{formatTime()}</h2>
     </div>
   )
 }
